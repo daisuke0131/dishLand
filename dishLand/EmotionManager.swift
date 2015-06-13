aa3a141bd31e75fa75dae5f0eb5bfdb55b120aaa
山本　恭大 <hogazurou_0225@yahoo.co.jp>
2015-06-13 19:51:50 +0900

    話すだけのdelegate追加

--- dishLand/EmotionManager.swift
+++ dishLand/EmotionManager.swift
@@ -11,11 +11,17 @@ import CoreMotion
 
 
 enum EMOTIONS {
+    //感情
     case normal // 通常時
     case glad //喜
     case anger // 怒
     case sad // 哀
     case happy // 楽
+    
+    // 話すだけ
+    case cheers //かんぱ〜い
+    case superSad //超かなし〜
+    case goodDrink // 良い飲みっぷりですな！
 }
 
 protocol EmotionManagerDelegate{
@@ -37,54 +43,26 @@ final class EmotionManager: NSObject {
     private var yCalibrate : Double = 0
     private var zCalibrate : Double = 0
     
+    private var isFirst = true
+    
     private let friq = 0.1 // 計測周期
     
-    private var counterMilSec = 0.0 {
-        willSet {
-        }
-        didSet {
-            //counterMilSec = counterMilSec * friq
-        }
-    }
+    private var counterMilSec = 0.0
     
     private let waitingTime = 50.0
     // 感情変化の時間（5s）
     private let threshold = 0.75
     private let thresholdBig = 1.2
     
-    private var xRawData : Double = 0{
-        willSet {
-        }
-        didSet {
-            xRawData = xRawData - xCalibrate
-        }
-    }
+    private var xRawData : Double = 0
     
-    private var yRawData : Double = 0{
-        willSet {
-        }
-        didSet {
-            yRawData = yRawData - yCalibrate
-        }
-    }
+    private var yRawData : Double = 0
     
-    private var zRawData : Double = 0{
-        willSet {
-        }
-        didSet {
-            zRawData = zRawData - zCalibrate
-        }
-    }
+    private var zRawData : Double = 0
     
     private var finishCalibrate = false;
     
-    private var yRotate:Bool = false
-    private var xRotate:Bool = false
-    private var zRotate:Bool = false
-    
-    //private var let dataMargine =
-    
-    
+    var drinkCount = 0 //短期間で連続して飲んだ時
     
     private override init() {
         super.init()
@@ -99,92 +77,98 @@ final class EmotionManager: NSObject {
         let handler:CMAccelerometerHandler = {(data:CMAccelerometerData!, error:NSError!) -> Void in
             
             //キャリブレーションデータ設定
-            if(self.finishCalibrate == false)
+            if(self.finishCalibrate == true)
             {
                 self.xCalibrate = data.acceleration.x
                 self.yCalibrate = data.acceleration.y
                 self.zCalibrate = data.acceleration.z
+                self.finishCalibrate = false
             }
             
-            self.xRawData = data.acceleration.x
-            self.yRawData = data.acceleration.y
-            self.zRawData = data.acceleration.z
-            /*
+            self.xRawData = data.acceleration.x - self.xCalibrate
+            self.yRawData = data.acceleration.y - self.yCalibrate
+            self.zRawData = data.acceleration.z - self.zCalibrate
+            
             println("\(self.xRawData)")
             println("\(self.yRawData)")
             println("\(self.zRawData)")
             println("\(self.counterMilSec)")
-            */
+            
             self.counterMilSec = self.counterMilSec + 1.0
             //===============================================
             
             var isMove = false
             var isBigMove = false
             
-            if(self.counterMilSec > self.waitingTime)
+            if(abs(self.xRawData) > self.thresholdBig)
             {
-                if(abs(self.xRawData) > self.thresholdBig)
-                {
-                    isBigMove = true
-                }
-                else if(abs(self.yRawData) > self.thresholdBig)
-                {
-                    isBigMove = true
-                }
-                else if(abs(self.zRawData) > self.thresholdBig)
-                {
-                    isBigMove = true
-                }
-                else if(abs(self.xRawData) > self.threshold)
-                {
-                    isMove = true
-                }
-                else if(abs(self.yRawData) > self.threshold)
-                {
-                    isMove = true
-                }
-                else if(abs(self.zRawData) > self.threshold)
-                {
-                    isMove = true
-                }
+                isBigMove = true
+            }
+            else if(abs(self.yRawData) > self.thresholdBig)
+            {
+                isBigMove = true
+            }
+            else if(abs(self.zRawData) > self.thresholdBig)
+            {
+                isBigMove = true
+            }
+            else if(abs(self.xRawData) > self.threshold)
+            {
+                isMove = true
+            }
+            else if(abs(self.yRawData) > self.threshold)
+            {
+                isMove = true
+            }
+            else if(abs(self.zRawData) > self.threshold)
+            {
+                isMove = true
             }
             
             if(isBigMove)
             {
-                self.delegate.changeEmotion(.happy)
-                self.emotion = .happy
+                if(self.isFirst)
+                {
+                    self.delegate.changeEmotion(.cheers)
+                    self.isFirst = false
+                }
+                else
+                {
+                    self.delegate.changeEmotion(.happy)
+                    self.emotion = .happy
+                }
                 self.counterMilSec = 0
             }
                 
-            else if(isMove == true)
+            else if(isMove)
             {
                 self.moveCount = self.moveCount + 1
-                switch(self.emotion)
-                {
-                case .normal:
-                    self.normalMove()
-                    break
-                case .anger:
-                    self.angerMove()
-                    break
-                case .glad:
-                    self.gladMove()
-                    break
-                case .happy:
-                    self.happyMove()
-                    break
-                case .sad:
-                    self.sadMove()
-                    break
-                default:
-                    self.normalMove()
-                    break
-                }
                 self.counterMilSec = 0 // 動いたらゼロに戻る
             }
             else
             {
                 // 動かない時限定の処理
+            }
+            switch(self.emotion)
+            {
+            case .normal:
+                self.normalMove()
+                break
+            case .anger:
+                self.angerMove()
+                break
+            case .glad:
+                self.gladMove()
+                break
+            case .happy:
+                self.happyMove()
+                break
+            case .sad:
+                self.sadMove()
+                break
+            default:
+                self.normalMove()
+                break
             }
             self.delegate.rawData(self.xRawData, y: self.yRawData, z: self.zRawData)
             
@@ -203,7 +187,6 @@ final class EmotionManager: NSObject {
             self.emotion = .sad
             self.counterMilSec = 0
         }
-        // 動き続けたらangerへ
     }
     
     private func angerMove()
@@ -218,9 +201,16 @@ final class EmotionManager: NSObject {
     
     private func sadMove()
     {
-        self.delegate.changeEmotion(.glad)
-        self.emotion = .glad
-        self.counterMilSec = 0
+        if(self.counterMilSec == 0)
+        {
+            self.delegate.changeEmotion(.glad)
+            self.emotion = .glad
+            self.counterMilSec = 0
+        }
+        else if(self.counterMilSec % waitingTime == 0)
+        {
+            self.delegate.changeEmotion(.superSad)
+        }
     }
     
     private func gladMove()
@@ -248,6 +238,8 @@ final class EmotionManager: NSObject {
     {
         finishCalibrate = true
         self.delegate.changeEmotion(.normal)
+        self.emotion = .normal
+        self.counterMilSec = 0
     }
     
     
