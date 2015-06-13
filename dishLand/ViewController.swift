f4f0235e4e99d9710653c9f1ba0bb448cc8936d3
Daisuke Yamashita <dai.tachikoma@gmail.com>
2015-06-13 14:35:04 +0900

    first commit

---
+++ dishLand/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dishLand
+//
+//  Created by daisuke on 2015/06/13.
+//  Copyright (c) 2015年 daisuke. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
