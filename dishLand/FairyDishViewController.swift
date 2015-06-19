c88946a93667f362e3d71d24c3b76430ead2fe3c
山本　恭大 <hogazurou_0225@yahoo.co.jp>
2015-06-19 23:17:45 +0900

    撮影機能削除

--- dishLand/FairyDishViewController.swift
+++ dishLand/FairyDishViewController.swift
@@ -37,7 +37,6 @@ class FairyDishViewController: UIViewController {
     var isSpeaking:Bool = false
     
     var players:[FairyPlayer] = [FairyPlayer]()
-    var camera: SecretCamera = SecretCamera()
     
     deinit{
         blinkTimer?.invalidate()
@@ -62,12 +61,6 @@ class FairyDishViewController: UIViewController {
         player.delegate = self
         player.playKanpai()
         players.append(player)
-        //after 1.0sec taking a picture
-        let delay = 1.0 * Double(NSEC_PER_SEC)
-        let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-        dispatch_after(time, dispatch_get_main_queue(), {
-            self.camera.openTake()
-        })
     }
     private func shouldEatVegetables(){
         startSpeaking()
